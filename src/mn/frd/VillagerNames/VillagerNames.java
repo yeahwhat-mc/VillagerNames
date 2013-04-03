a3e669f6614acc5884107a8769b0a2373568d672
Jonas Friedmann <j@frd.mn>
2013-04-03 19:19:17 +0200

    v0.2 - new feature: different names per villager profession

--- src/mn/frd/VillagerNames/VillagerNames.java
+++ src/mn/frd/VillagerNames/VillagerNames.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.util.List;
 
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Villager;
+import org.bukkit.entity.Villager.Profession;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
@@ -41,16 +43,83 @@ public class VillagerNames extends JavaPlugin implements Listener {
 		// Check if Creature is a VILLAGER
 		if(event.getEntityType() == EntityType.VILLAGER)
 		{
-			// Load the names out of config.yml into a List
-			List<?> names = this.getConfig().getList("names");
-			// Shuffle that list
-			java.util.Collections.shuffle(names);
-			// Get the first element of the list
-			String randomname = names.get(0).toString();
+			// Check if event is instance of Villager
+			if(event.getEntity() instanceof org.bukkit.entity.Villager){
+				// Store the current profession in variable
+				Profession currentProfession = ((Villager) event.getEntity()).getProfession();
+				
+				this.getLogger().info("VN: Profession - " + currentProfession.getId());
+				
+				// If profession == FARMER
+				if (currentProfession.getId() == 0){
+					// Load the FARMER names out of config.yml into a List
+					List<?> names = this.getConfig().getList("farmer");
+					// Shuffle that list
+					java.util.Collections.shuffle(names);
+					// Get the first element of the list
+					String randomname = names.get(0).toString();
 
-			// Set the custom name of the Villager
-			event.getEntity().setCustomName(randomname);
-			event.getEntity().setCustomNameVisible(true);
+					// Set the custom name of the Villager
+					event.getEntity().setCustomName(randomname);
+					event.getEntity().setCustomNameVisible(true);
+				}
+				
+				// If profession == LIBRARIAN
+				if (currentProfession.getId() == 1){
+					// Load the LIBRARIAN names out of config.yml into a List
+					List<?> names = this.getConfig().getList("librarian");
+					// Shuffle that list
+					java.util.Collections.shuffle(names);
+					// Get the first element of the list
+					String randomname = names.get(0).toString();
+
+					// Set the custom name of the Villager
+					event.getEntity().setCustomName(randomname);
+					event.getEntity().setCustomNameVisible(true);
+				}
+				
+				// If profession == PRIEST
+				if (currentProfession.getId() == 2){
+					// Load the PRIEST names out of config.yml into a List
+					List<?> names = this.getConfig().getList("priest");
+					// Shuffle that list
+					java.util.Collections.shuffle(names);
+					// Get the first element of the list
+					String randomname = names.get(0).toString();
+
+					// Set the custom name of the Villager
+					event.getEntity().setCustomName(randomname);
+					event.getEntity().setCustomNameVisible(true);
+				}
+				
+				// If profession == BLACKSMITH
+				if (currentProfession.getId() == 3){
+					// Load the BLACKSMITH names out of config.yml into a List
+					List<?> names = this.getConfig().getList("blacksmith");
+					// Shuffle that list
+					java.util.Collections.shuffle(names);
+					// Get the first element of the list
+					String randomname = names.get(0).toString();
+
+					// Set the custom name of the Villager
+					event.getEntity().setCustomName(randomname);
+					event.getEntity().setCustomNameVisible(true);
+				}
+				
+				// If profession == BUTCHER
+				if (currentProfession.getId() == 4){
+					// Load the BUTCHER names out of config.yml into a List
+					List<?> names = this.getConfig().getList("butcher");
+					// Shuffle that list
+					java.util.Collections.shuffle(names);
+					// Get the first element of the list
+					String randomname = names.get(0).toString();
+
+					// Set the custom name of the Villager
+					event.getEntity().setCustomName(randomname);
+					event.getEntity().setCustomNameVisible(true);
+				}
+			}
 		}
 	}
 }
